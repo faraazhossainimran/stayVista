ba31127b06d8d28de2ae94d48f814951fe7816ad
Dev Enthusiastic <faraazhossainimran@gmail.com>
2023-12-05 01:23:38 +0600

    card and categories design added

---
+++ client/src/components/Rooms/Categories/Categories.jsx
@@ -0,0 +1,21 @@
+import Container from "../../Shared/Container";
+import { categories } from "./CategoriesData";
+import CategoryBox from "./CategoryBox";
+
+const Categories = () => {
+  return (
+    <Container>
+      <div className="pt-4 flex items-center justify-between overflow-x-auto">
+        {categories.map((category) => (
+          <CategoryBox
+            key={category.label}
+            category={category}
+            icon={category.icon}
+          ></CategoryBox>
+        ))}
+      </div>
+    </Container>
+  );
+};
+
+export default Categories;
---
+++ client/src/components/Rooms/Categories/CategoryBox.jsx
@@ -0,0 +1,11 @@
+const CategoryBox = ({ category }) => {
+  const { label, icon: Icon } = category;
+  return (
+    <div className="flex flex-col items-center justify-center gap-2 p-3 border-b-2 hover:text-neutral-800 transition cursor-pointer">
+      <Icon></Icon>
+      <div className="text-sm font-medium">{label}</div>
+    </div>
+  );
+};
+
+export default CategoryBox;
